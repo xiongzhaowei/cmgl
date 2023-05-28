8ef3ed97eae5db2b74819e2d3c2d69a14510f450
熊朝伟 <xiongzw@ushareit.com>
2023-05-29 03:27:10 +0800

    修复angle编译脚本只拉取不编译的问题。

--- windows-build-angle.cmd
+++ windows-build-angle.cmd
@@ -11,7 +11,10 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 set PATH=%~dp0build\depot_tools;%PATH%
 
 cd %~dp0build\src\angle
-fetch angle
-gn gen out/Release --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false"
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+call fetch angle
+call git checkout chromium/5776
+call gclient sync || exit /b 1
+call gn gen out/Release --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false"
 autoninja -C out/Release libEGL libGLESv2 libGLESv1_CM
 
