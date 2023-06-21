bf4a1c9c87babc86d08ac9d326b09057c8799328
熊朝伟 <xiongzw@ushareit.com>
2023-06-21 22:32:49 +0800

    完成AudioSplitter类，正确处理音频样本切分。

--- examples/windows/encode/main.cpp
+++ examples/windows/encode/main.cpp
@@ -21,102 +21,116 @@ const std::string wcstombs(const std::wstring& wstr, uint32_t codePage) {
 using namespace wheel;
 using namespace wheel::ffmpeg;
 
-template <typename T>
-class AudioSplitter : public Transformer<Frame> {
+template <typename T, bool planar>
+class AudioSplitterT : public Transformer<Frame> {
     RefPtr<Consumer<Frame>> _output;
     AVSampleFormat _format;
-    AVChannelLayout _ch_layout;
-    bool _planar;
-    int32_t _nb_samples;
-    int32_t _pts = 0;
-    size_t _buffer_size = 0;
-    std::vector<std::vector<T>> _buffers;
+    AVChannelLayout _layout;
+    RefPtr<Frame> _frame;
+    int32_t _sampleCount;
+    int32_t _sampleTotal;
+    int64_t _timestamp;
 public:
-    AudioSplitter(RefPtr<Consumer<Frame>> output, AVSampleFormat format, AVChannelLayout ch_layout, bool planar, int32_t nb_samples) : _output(output), _format(format), _ch_layout(ch_layout), _planar(planar), _nb_samples(nb_samples) {
+    AudioSplitterT(RefPtr<Consumer<Frame>> output, AVSampleFormat format, AVChannelLayout ch_layout, int32_t nb_samples) : _output(output), _format(format), _layout(ch_layout), _sampleTotal(nb_samples), _timestamp(0) {
         assert(output != nullptr);
         assert(format != AV_SAMPLE_FMT_NONE);
         assert(ch_layout.nb_channels > 0);
         assert(nb_samples > 0);
-        if (planar) {
-            _buffers.resize(ch_layout.nb_channels);
-            for (std::vector<T>& buffer : _buffers) {
-                buffer.reserve(_nb_samples);
+    }
+    void copyData(uint8_t** data, int32_t offset, int32_t count, int32_t nb_channels) {
+        if (_frame == nullptr) {
+            _frame = Frame::alloc(_format, _layout, _sampleTotal);
+            _sampleCount = 0;
+        }
+        if (_sampleCount + count >= _sampleTotal) {
+            int32_t copyCount = _sampleTotal - _sampleCount;
+            copy(data, offset, copyCount, nb_channels);
+            offset += copyCount;
+            count -= copyCount;
+
+            _frame->frame()->pts = _timestamp;
+            _output->add(_frame);
+            _frame = nullptr;
+
+            if (count > 0) {
+                copyData(data, offset, count, nb_channels);
             }
         } else {
-            _buffers.resize(1);
-            _buffers.back().reserve(nb_samples * ch_layout.nb_channels);
+            copy(data, offset, count, nb_channels);
         }
     }
-    void add(RefPtr<Frame> frame) override {
-        assert(av_channel_layout_compare(&_ch_layout, &frame->frame()->ch_layout) == 0);
-        assert(_format == (AVSampleFormat)frame->frame()->format);
-
-        size_t nb_samples = frame->frame()->nb_samples;
-        if (_buffer_size == 0 && nb_samples == _nb_samples) {
-            _output->add(frame);
-            return;
-        }
-
-        int32_t nb_channels = _ch_layout.nb_channels;
-        if (_buffer_size + nb_samples >= _nb_samples) {
-            RefPtr<Frame> output = Frame::alloc(_format, _ch_layout, _nb_samples);
-
-            size_t offset = _nb_samples - _buffer_size;
-            if (_planar) {
-                for (int channel = 0; channel < nb_channels; channel++) {
-                    assert(_buffers[channel].size() == _buffer_size);
-
-                    memcpy(output->frame()->extended_data[channel], _buffers[channel].data(), _buffer_size * sizeof(T));
-                    memcpy(output->frame()->extended_data[channel] + _buffer_size, frame->frame()->extended_data[channel], offset * sizeof(T));
-                }
-            } else {
-                memcpy(output->frame()->extended_data[0], _buffers[0].data(), _buffer_size * nb_channels * sizeof(T));
-                memcpy(output->frame()->extended_data[0] + _buffer_size * nb_channels, frame->frame()->extended_data[0], offset * nb_channels * sizeof(T));
-            }
-            _output->add(output);
-            while (nb_samples - offset > _nb_samples) {
-                output = Frame::alloc(_format, _ch_layout, _nb_samples);
-                if (_planar) {
-                    for (int channel = 0; channel < nb_channels; channel++) {
-                        memcpy(output->frame()->extended_data[channel], frame->frame()->extended_data[channel] + offset, _nb_samples * sizeof(T));
-                    }
-                } else {
-                    memcpy(output->frame()->extended_data[0], frame->frame()->extended_data[0] + offset * nb_channels, _nb_samples * nb_channels * sizeof(T));
-                }
-                _output->add(output);
-                offset += _nb_samples;
-            }
-            if (nb_samples - offset > 0) {
-                _buffer_size = nb_samples - offset;
-                if (_planar) {
-                    for (int channel = 0; channel < nb_channels; channel++) {
-                        _buffers[channel].resize(_buffer_size);
-                        memcpy(_buffers[channel].data(), frame->frame()->extended_data[channel] + offset, _buffer_size * sizeof(T));
-                    }
-                } else {
-                    _buffers[0].resize(_buffer_size * nb_channels);
-                    memcpy(_buffers[0].data(), frame->frame()->extended_data[0] + offset * nb_channels, _buffer_size * nb_channels * sizeof(T));
-                }
+    void copy(uint8_t** data, int32_t offset, int32_t count, int32_t nb_channels) {
+        if (planar) {
+            for (int channel = 0; channel < nb_channels; channel++) {
+                memcpy(_frame->frame()->extended_data[channel] + _sampleCount * sizeof(T), data[channel] + offset * sizeof(T), count * sizeof(T));
             }
         } else {
-            if (_planar) {
-                for (int channel = 0; channel < nb_channels; channel++) {
-                    _buffers[channel].resize(_buffer_size + nb_samples);
-                    memcpy(_buffers[channel].data() + _buffer_size, frame->frame()->extended_data[channel], nb_samples * sizeof(T));
-                }
-            } else {
-                size_t size = _buffer_size * nb_channels;
-                _buffers[0].resize(size);
-                memcpy(_buffers[0].data() + size, frame->frame()->extended_data[0], nb_samples * nb_channels * sizeof(T));
-            }
-            _buffer_size += nb_samples;
+            memcpy(_frame->frame()->extended_data[0] + _sampleCount * nb_channels * sizeof(T), data[0] + offset * nb_channels * sizeof(T), count * nb_channels * sizeof(T));
         }
+        _timestamp += count;
+        _sampleCount += count;
+    }
+    void add(RefPtr<Frame> frame) override {
+        assert(av_channel_layout_compare(&_layout, &frame->frame()->ch_layout) == 0);
+        assert(_format == (AVSampleFormat)frame->frame()->format);
+
+        copyData(frame->frame()->extended_data, 0, frame->frame()->nb_samples, frame->frame()->ch_layout.nb_channels);
     }
     void addError() override {
         _output->addError();
     }
     void close() override {
         _output->close();
+    }
+};
+
+class AudioSplitter : public Transformer<Frame> {
+    RefPtr<Consumer<Frame>> _output;
+public:
+    AudioSplitter(RefPtr<Consumer<Frame>> output, AVSampleFormat format, AVChannelLayout ch_layout, int32_t frame_size) {
+        _output = build(output, format, ch_layout, frame_size);
+    }
+    AudioSplitter(RefPtr<Consumer<Frame>> output, AVCodecContext* context) {
+        _output = build(output, context->sample_fmt, context->ch_layout, context->frame_size);
+    }
+    void add(RefPtr<Frame> frame) override {
+        _output->add(frame);
+    }
+    void addError() override {
+        _output->addError();
+    }
+    void close() override {
+        _output->close();
+    }
+    static RefPtr<Transformer<Frame>> build(RefPtr<Consumer<Frame>> output, AVSampleFormat format, AVChannelLayout ch_layout, int32_t nb_samples) {
+        switch (format) {
+        case AV_SAMPLE_FMT_U8:
+            return new AudioSplitterT<uint8_t, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S16:
+            return new AudioSplitterT<int16_t, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S32:
+            return new AudioSplitterT<int32_t, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_FLT:
+            return new AudioSplitterT<float, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_DBL:
+            return new AudioSplitterT<double, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_U8P:
+            return new AudioSplitterT<uint8_t, true>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S16P:
+            return new AudioSplitterT<int16_t, true>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S32P:
+            return new AudioSplitterT<int32_t, true>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_FLTP:
+            return new AudioSplitterT<float, true>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_DBLP:
+            return new AudioSplitterT<double, true>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S64:
+            return new AudioSplitterT<int64_t, false>(output, format, ch_layout, nb_samples);
+        case AV_SAMPLE_FMT_S64P:
+            return new AudioSplitterT<int64_t, true>(output, format, ch_layout, nb_samples);
+        default:
+            return nullptr;
+        }
     }
 };
 
@@ -159,12 +173,7 @@ int main() {
         RefPtr<Converter> converter = ffmpeg::AudioConverter::create(audioSource->stream(), audioTarget->context()->sample_fmt, audioTarget->context()->ch_layout);
         RefPtr<ffmpeg::Stream<ffmpeg::Frame>> audioFilter = audioSource->convert<ffmpeg::Frame>([converter](RefPtr<ffmpeg::Frame> frame) {
             return converter->convert(frame);
-        })->transform<AudioSplitter<int16_t>>(
-            audioTarget->context()->sample_fmt,
-            audioTarget->context()->ch_layout,
-            false,
-            audioTarget->context()->frame_size
-        );
+        })->transform<AudioSplitter>(audioTarget->context());
         audioFilter->listen(audioTarget);
 
         RefPtr<ffmpeg::Stream<ffmpeg::Frame>> videoFilter = videoSource->convert<ffmpeg::Frame>([](RefPtr<ffmpeg::Frame> frame) {
